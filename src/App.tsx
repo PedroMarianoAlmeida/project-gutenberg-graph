2c84d316d8c9ce6b90a686a30c50e1600d5681e4
Pedro Almeida <pedroalmeidaprogrammer@gmail.com>
2025-03-26 18:46:04 -0300

    add form from docs

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import { ThemeProvider } from "@/components/theme-provider";
 import "./App.css";
 
+import { BookForm } from "@/components/BookForm";
+
 function App() {
   return (
     <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
-      <></>
+      <BookForm />
     </ThemeProvider>
   );
 }
