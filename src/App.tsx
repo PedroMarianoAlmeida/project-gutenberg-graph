c3dd11bdb22ae5663e7041f32e30b6ac9c67835e
Pedro Almeida <pedroalmeidaprogrammer@gmail.com>
2025-03-26 18:36:14 -0300

    add dark mode

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,15 @@
 import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
-import { Button } from "./../src/components/ui/button";
+import { Button } from "@/components/ui/button";
+import { ThemeProvider } from "@/components/theme-provider"
 import "./App.css";
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
-    <>
+    <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
       <div>
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
@@ -29,7 +30,7 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
-    </>
+    </ThemeProvider>
   );
 }
 
