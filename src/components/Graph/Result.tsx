43effb36691731c9964da97a6082d86aedb0a2d4
Pedro Almeida <pedroalmeidaprogrammer@gmail.com>
2025-03-27 00:21:47 -0300

    graph rendered

---
+++ src/components/Graph/Result.tsx
@@ -0,0 +1,57 @@
+import ForceGraph2D, { NodeObject } from "react-force-graph-2d";
+import mockedData from "@/graphDataExample.json";
+
+type CustomNode = NodeObject & {
+  id: string;
+  x: number;
+  y: number;
+  __bckgDimensions?: [number, number];
+};
+
+export const Result = () => {
+  return (
+    <div className="flex justify-center items-center">
+      <ForceGraph2D
+        graphData={mockedData}
+        enablePanInteraction={false}
+        linkColor={() => "#00BFFF"}
+        nodeCanvasObject={(node, ctx, globalScale) => {
+          const customNode = node as CustomNode;
+          const label = customNode.id;
+          const fontSize = 12 / globalScale;
+          ctx.font = `${fontSize}px Sans-Serif`;
+          const textWidth = ctx.measureText(label).width;
+          const bckgDimensions: [number, number] = [textWidth, fontSize].map(
+            (n) => n + fontSize * 0.2
+          ) as [number, number];
+
+          ctx.fillStyle = "rgba(0, 0, 0, 0.6)";
+          ctx.fillRect(
+            customNode.x - bckgDimensions[0] / 2,
+            customNode.y - bckgDimensions[1] / 2,
+            ...bckgDimensions
+          );
+
+          ctx.textAlign = "center";
+          ctx.textBaseline = "middle";
+          ctx.fillStyle = "#FFFFFF";
+          ctx.fillText(label, customNode.x, customNode.y);
+
+          customNode.__bckgDimensions = bckgDimensions;
+        }}
+        nodePointerAreaPaint={(node, color, ctx) => {
+          const customNode = node as CustomNode;
+          const bckgDimensions = customNode.__bckgDimensions;
+          if (bckgDimensions) {
+            ctx.fillStyle = color;
+            ctx.fillRect(
+              customNode.x - bckgDimensions[0] / 2,
+              customNode.y - bckgDimensions[1] / 2,
+              ...bckgDimensions
+            );
+          }
+        }}
+      />
+    </div>
+  );
+};
